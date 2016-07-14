967a74fdc68e38d5a25724351158abae32232424
bighead <longming_xu@126.com>
2016-07-14 18:13:02 +0800

    修改Unity客户端：增加登录实现

--- TestPhotonGame/UnityClient/Assets/Scripts/PhotonClient.cs
+++ TestPhotonGame/UnityClient/Assets/Scripts/PhotonClient.cs
@@ -19,20 +19,25 @@ public class PhotonClient : MonoBehaviour, IPhotonPeerListener
     private PhotonPeer m_Peer;
     private bool m_ConnectRet = false;   // 连接返回码
     private bool m_ServerConnected = false; // 连接状态
+    private bool m_Login = false; // 是否登录
 
     void Awake()
-    {
-        this.m_Peer = new PhotonPeer(this, ConnectionProtocol.Udp);
+    {       
         this.m_ConnectRet = false;
         this.m_ServerConnected = false;
+        this.m_Login = false;
     }
 
     // Use this for initialization
     void Start()
     {
-        bool ret = this.m_Peer.Connect(this.serverAddress, this.serverApplication);
+        // Client和Server进行握手连接
+        this.m_Peer = new PhotonPeer(this, ConnectionProtocol.Udp);
+        bool ret = this.m_Peer.Connect(this.serverAddress, this.serverApplication); // (如果ip可达，并且可连,返回true)
         if (ret)
         {
+            Debug.Log("ip可达，并且可连....");
+            this.ui_InfoPanel.text += string.Format("\n<color=green>{0}</color>", "ip可达，并且可连....");
             this.m_ConnectRet = true;
         }
         else
@@ -45,16 +50,64 @@ public class PhotonClient : MonoBehaviour, IPhotonPeerListener
     // Update is called once per frame
     void Update()
     {
+        // Client与Server是否握手成功，建立Socket通信通道
         if (this.m_ConnectRet)
         {
-            // 启动点对点通信监听，当Server端有信息传过来的时候，根据信息的类型会触发OnEvent,OnOperationResponse
+            // 启动点对点通信监听，当Server端有信息传过来的时候，根据信息的类型会触发OnEvent,OnOperationResponse（只有调用Service,SDK封装的网络接口才会通知业务逻辑层，就是实现的IPhotonPeerListener接口）
             this.m_Peer.Service();
-            Debug.Log("<color=yellow>...</color>");
+            //Debug.Log("<color=yellow>...</color>");
             //this.ui_InfoPanel.text += "<color=yellow>...</color>";
         }
     }
 
+    void OnGUI()
+    {
+        GUI.Label(new Rect(30, 10, 400, 20), "Test Unity Photon Client");
 
+        if (this.m_ServerConnected )
+        {
+            GUI.Label(new Rect(30, 30, 400, 20), "Connected");
+
+            // 是否登录
+            if (this.m_Login)
+            {
+                GUI.Label(new Rect(30, 70, 400, 20), "Login Success");
+            }
+            else
+            {
+                GUI.Label(new Rect(30, 70, 400, 20), "Please Login");
+            }
+        }
+        else
+        {
+            GUI.Label(new Rect(30, 30, 400, 20), "DisConnected");
+
+            if (GUI.Button(new Rect(30, 50, 100, 20), "重连"))
+            {
+                if (!this.m_ConnectRet)
+                {
+                    bool ret = this.m_Peer.Connect(this.serverAddress, this.serverApplication);
+                    if (ret)
+                    {
+                        Debug.Log("ip可达，并且可连....");
+                        this.ui_InfoPanel.text += string.Format("\n<color=green>{0}</color>", "ip可达，并且可连....");
+                        this.m_ConnectRet = true;
+                    }
+                    else
+                    {
+                        Debug.LogError("Unknown serverAddress!");
+                        this.ui_InfoPanel.text += string.Format("\n<color=red>{0}</color>", "Unknown serverAddress!");
+                    }
+                }                
+            }
+        }
+
+
+        if (GUI.Button(new Rect(30, 120, 100, 20), "Clear Log"))
+        {
+            this.ui_InfoPanel.text = "";
+        }
+    }
 
     public PhotonPeer GetClientPeer()
     {
@@ -64,24 +117,37 @@ public class PhotonClient : MonoBehaviour, IPhotonPeerListener
 
     public void LoginClick()
     {
-        if (this.ui_AccountInput.text != "" && this.ui_PasswordInput.text != "")
+        if (!this.m_Login)
         {
-            Dictionary<byte, object> param = new Dictionary<byte, object>();
-            param.Add((byte)1, this.ui_AccountInput.text.Trim());
-            param.Add((byte)2, this.ui_PasswordInput.text.Trim());
+            if (this.ui_AccountInput.text != "" && this.ui_PasswordInput.text != "")
+            {
+                Dictionary<byte, object> param = new Dictionary<byte, object>();
+                param.Add((byte)1, this.ui_AccountInput.text.Trim());
+                param.Add((byte)2, this.ui_PasswordInput.text.Trim());
 
-            // 向Server端发送信息（命令代码暂定5）
-            this.m_Peer.OpCustom(5, param, true);
+                // 向Server端发送信息（命令代码暂定5）
+                this.m_Peer.OpCustom(5, param, true);
+            }   
+        }
+        else
+        {
+            Debug.Log("已经登录");
+            this.ui_InfoPanel.text += "\n已经登录";
         }
     }
 
 
 
     #region >>>>>>>>>>实现IPhotonPeerListener接口中的方法<<<<<<<<<<
+    /***
+     * 
+     * 这几个接口（业务逻辑处理），实际上是SDK的回调，只有调用m_Peer的Service方法，业务逻辑才能被调用
+     * 
+     ***/
     public void DebugReturn(DebugLevel level, string message)
     {
         Debug.Log("DebugReturn:" + level + "----" + message);
-        this.ui_InfoPanel.text += string.Format("\n<color=green>{0}</color>", "DebugReturn:" + level + "----" + message);
+        this.ui_InfoPanel.text += string.Format("\n<color=red>{0}</color>", "DebugReturn:" + level + "----" + message);
     }
 
     public void OnEvent(EventData eventData)
@@ -100,6 +166,7 @@ public class PhotonClient : MonoBehaviour, IPhotonPeerListener
                 switch (operationResponse.ReturnCode)
                 {
                     case (short)0:
+                        this.m_Login = true;
                         int ret = Convert.ToInt16(operationResponse.Parameters[(byte)80]);
                         string account = Convert.ToString(operationResponse.Parameters[(byte)1]);
                         string password = Convert.ToString(operationResponse.Parameters[(byte)2]);
@@ -142,6 +209,7 @@ public class PhotonClient : MonoBehaviour, IPhotonPeerListener
                 Debug.Log("连接失败....");
                 this.ui_InfoPanel.text += string.Format("\n<color=green>{0}</color>", "连接失败....");
                 this.m_ServerConnected = false;
+                this.m_ConnectRet = false;               
                 break;
             case StatusCode.DisconnectByServer:
                 break;
